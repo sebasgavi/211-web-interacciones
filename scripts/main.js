f0925ba2a05913062bb89c5c1b0a4f9e778d27c2
Sebastián Gaviria <sebastiangaviria@outlook.com>
2021-03-12 09:03:30 -0500

    carrousel

--- scripts/main.js
+++ scripts/main.js
@@ -17,3 +17,32 @@ for(let i = 0; i < thumbs.length; i++) {
   // cuando sea la primera iteración llamamos la función que cambia el source
   if(i === 0) handleThumbClick();
 }
+
+
+
+
+const slider = document.querySelector('.carrousel__slider');
+let currentSlide = 0;
+
+function handleInterval () {
+  currentSlide++;
+  if(currentSlide >= slider.children.length){
+    currentSlide = 0;
+  }
+  slider.style.transform = `translate(-${ slider.clientWidth * currentSlide }px, 0px)`;
+}
+
+let intervalId;
+
+function handleMouseEnter () {
+  intervalId = setInterval(handleInterval, 1000);
+}
+
+slider.addEventListener('mouseenter', handleMouseEnter);
+
+
+function handleMouseLeave () {
+  clearInterval(intervalId);
+}
+
+slider.addEventListener('mouseleave', handleMouseLeave);
