f3cdfc3961172b341a5d0c58e4169db4428f77d1
Sebastián Gaviria <sebastiangaviria@outlook.com>
2021-04-09 09:33:34 -0500

    form

--- scripts/productForm.js
+++ scripts/productForm.js
@@ -10,11 +10,43 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
+const storage = firebase.storage();
 
 const productForm = document.querySelector('.productForm');
 const productFormLoader = document.querySelector('.productForm__loader');
 const productFormSuccess = document.querySelector('.productForm__success');
 const productFormError = document.querySelector('.productForm__error');
+const productFormImg = document.querySelector('.productForm__img');
+
+const tshirtFields = document.querySelector('.tshirtFields');
+const stickerFields = document.querySelector('.stickerFields');
+const mugFields = document.querySelector('.mugFields');
+
+productForm.type.addEventListener('change', function() {
+  tshirtFields.classList.add('hidden');
+  stickerFields.classList.add('hidden');
+  mugFields.classList.add('hidden');
+  switch(productForm.type.value) {
+    case 'sticker':
+      stickerFields.classList.remove('hidden');
+      break;
+    case 't-shirt':
+      tshirtFields.classList.remove('hidden');
+      break;
+    case 'mug':
+      mugFields.classList.remove('hidden');
+      break;
+  }
+});
+
+productForm.image.addEventListener('change', function () {
+  var reader = new FileReader();
+  reader.onload = function(event) {
+    productFormImg.classList.remove('hidden');
+    productFormImg.setAttribute('src', event.target.result);
+  }
+  reader.readAsDataURL(productForm.image.files[0]); // convert to base64 string
+});
 
 productForm.addEventListener('submit', function (event) {
   event.preventDefault();
@@ -22,23 +54,75 @@ productForm.addEventListener('submit', function (event) {
   const product = {
     name: productForm.name.value,
     price: parseFloat(productForm.price.value),
-    sizes: [],
+    type: productForm.type.value,
   };
-  if(productForm.size_s.checked) product.sizes.push('s');
-  if(productForm.size_m.checked) product.sizes.push('m');
-  if(productForm.size_l.checked) product.sizes.push('l');
 
-  console.log(product);
+  switch(product.type) {
+    case 'sticker':
+      product.sizes = [];
+      if(productForm.size_10.checked) product.sizes.push(10);
+      if(productForm.size_20.checked) product.sizes.push(20);
+      break;
+    case 't-shirt':
+      product.sizes = [];
+      if(productForm.size_s.checked) product.sizes.push('s');
+      if(productForm.size_m.checked) product.sizes.push('m');
+      if(productForm.size_l.checked) product.sizes.push('l');
+      break;
+    case 'mug':
+      product.color = productForm.color.value;
+  }
 
-  productFormLoader.classList.remove('hidden');
-  db.collection('products').add(product)
-  .then(function (docRef) {
-    console.log('document added', docRef.id)
-    productFormLoader.classList.add('hidden');
-    productFormSuccess.classList.remove('hidden');
-  })
-  .catch(function (error) {
-    productFormLoader.classList.add('hidden');
+  let error = '';
+  if(!product.name) {
+    error += 'The product name is required. <br/>';
+  }
+  if(!product.price) {
+    error += 'The product price is required. <br/>';
+  }
+  if(product.price < 1000) {
+    error += 'The product price can\'t be less than 1000. <br/>';
+  }
+  if(!product.type) {
+    error += 'You must select a product type. <br/>';
+  }
+  if(error) {
+    productFormError.innerHTML = error;
     productFormError.classList.remove('hidden');
+    // return;
+  } else {
+    productFormError.classList.add('hidden');
+  }
+
+  const file = productForm.image.files[0];
+
+  var storageRef = firebase.storage().ref();
+  var fileRef = storageRef.child(`images/${product.type}/${file.name}`);
+
+  // espera a subir la imagen
+  fileRef.put(file).then(function (snapshot) {
+
+    // espera a obtener la url de descarga de la imagen
+    snapshot.ref.getDownloadURL().then((downloadURL) => {
+      productFormLoader.classList.remove('hidden');
+      product.imageUrl = downloadURL;
+      product.imageRef = snapshot.ref.fullPath;
+
+      // espera a subir la información al firestore
+      db.collection('products').add(product)
+      .then(function (docRef) {
+        console.log('document added', docRef.id)
+        productFormLoader.classList.add('hidden');
+        productFormSuccess.classList.remove('hidden');
+      })
+      .catch(function (error) {
+        productFormLoader.classList.add('hidden');
+        productFormError.classList.remove('hidden');
+      });
+      console.log('File available at', downloadURL);
+    });
+
+    console.log(snapshot)
+    console.log('Uploaded a blob or file!');
   });
 });
